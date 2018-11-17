5e13e959dcf3825b178bb7eaa09c5bcefb2d87a0
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-17 12:27:06 +0100

    Create nom.java

---
+++ src/main/java/Commands/Kawaii/nom.java
@@ -0,0 +1,43 @@
+package Commands.Kawaii;
+
+import Commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class nom implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+        List argsList = Arrays.asList(args);
+        StringBuilder sb = new StringBuilder();
+        argsList.forEach(s -> sb.append(s + " "));
+        send(sb.toString(), event);
+
+        //Links
+
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+    private void send(String msg, MessageReceivedEvent event) {
+        if (msg.length() < 1) {
+            event.getTextChannel().sendMessage(
+                    new EmbedBuilder().setColor(Color.RED)
+                            .setDescription("Please enter a word or mention an User").build()).queue();
+        }else
+            event.getTextChannel().sendMessage(
+                    new EmbedBuilder().setColor(Color.PINK)
+                            .setDescription("**" + msg + "**, you got a nom! from **" + event.getMessage().getAuthor().getName() + "!** :wink:")
+                            .setImage("https://cdn.weeb.sh/images/HkGGy1Yvb.gif").build()).queue();
+    }
+}
