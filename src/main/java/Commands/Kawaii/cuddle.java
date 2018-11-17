d754bda4fb7c608888f818f6677e29dafeff76a1
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-17 12:25:38 +0100

    Create cuddle.java

---
+++ src/main/java/Commands/Kawaii/cuddle.java
@@ -0,0 +1,48 @@
+package Commands.Kawaii;
+
+import Commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class cuddle implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+        List argsList = Arrays.asList(args);
+        StringBuilder sb = new StringBuilder();
+        argsList.forEach(s -> sb.append(s + " "));
+        send(sb.toString(), event);
+
+        //Links
+
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+
+    private void send(String msg, MessageReceivedEvent event) {
+        if (msg.length() < 1) {
+            event.getTextChannel().sendMessage(
+                    new EmbedBuilder().setColor(Color.RED)
+                            .setDescription("Please enter a word or mention an User").build()).queue();
+
+        } else{
+            event.getTextChannel().sendMessage(
+                    new EmbedBuilder().setColor(Color.PINK)
+                            .setDescription("**" + msg + "**, you have been cuddled by **" + event.getMessage().getAuthor().getName() + "!** :3")
+                            .setImage("https://cdn.discordapp.com/attachments/404334704804888577/408344769824292876/image.gif").build()).queue();
+        }
+    }
+
+}
+
