9d980926e626c324c36251eaddf07cea5d31b871
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-17 12:17:59 +0100

    Create RegisterListener.java

---
+++ src/main/java/Listeners/Loader/RegisterListener.java
@@ -0,0 +1,20 @@
+package Listeners.Loader;
+
+import Listeners.Announcements.MemberChangeListener;
+import Listeners.Cmd.CommandListener;
+import Listeners.Logs.AuditLogListener;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class RegisterListener extends ListenerAdapter {
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        event.getJDA().addEventListener(new CommandListener());
+        event.getJDA().addEventListener(new AuditLogListener());
+        event.getJDA().addEventListener(new MemberChangeListener());
+        event.getJDA().setAutoReconnect(true);
+        System.out.println("Finished Loading!\n" +
+                            "Added " + event.getJDA().getRegisteredListeners().size() + " Listener(s)");
+    }
+}
