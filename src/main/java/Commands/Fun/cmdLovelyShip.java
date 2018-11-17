89577cce283727f922abd74223a134e5b59a71f9
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-17 12:24:19 +0100

    Create cmdLovelyShip.java

---
+++ src/main/java/Commands/Fun/cmdLovelyShip.java
@@ -0,0 +1,39 @@
+package Commands.Fun;
+
+import Commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import java.util.Random;
+
+public class cmdLovelyShip implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+        Random rate = new Random();
+        int y = rate.nextInt(101);
+      if(event.getMessage().getMentionedMembers().size() == 0) {
+          event.getTextChannel().sendMessage("You need to specify your request!").queue();
+      }else {
+          event.getTextChannel().sendMessage(
+
+                  new EmbedBuilder()
+                          .setTitle("Lovely shipping " + event.getMessage().getAuthor().getName() + " and " + event.getMessage().getMentionedMembers().get(0).getEffectiveName())
+                          .addField("Shipability", "``" + y + "%``", false)
+                  .build()
+
+          ).queue();
+      }
+
+    }
+
+    @Override
+    public void executed(boolean sucess, MessageReceivedEvent event) {
+    }
+
+
+}
