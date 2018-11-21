a5046db104b0a55665536d11d7a3c0c78bd819da
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-21 17:34:22 +0100

    Update CommandListener.java

--- src/main/java/Listeners/Cmd/CommandListener.java
+++ src/main/java/Listeners/Cmd/CommandListener.java
@@ -9,10 +9,7 @@ import org.discordbots.api.client.DiscordBotListAPI;
 import java.util.Objects;
 
 public class CommandListener extends ListenerAdapter {
-    DiscordBotListAPI api = new DiscordBotListAPI.Builder()
-            .token(System.getenv("DB_TOKEN"))
-            .botId("406097711603908621")
-            .build();
+
     public void onMessageReceived(MessageReceivedEvent event) {
 
             if (event.getMessage().getContentRaw().startsWith(STATIC.PREFIX)
@@ -23,22 +20,6 @@ public class CommandListener extends ListenerAdapter {
             } else {
                 return;
             }
-
-        String userId = event.getAuthor().getId(); // ID of the user you're checking
-        api.hasVoted(userId).whenComplete((hasVoted, e) -> {
-            if(hasVoted) {
-                if(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).size() == 0) {
-                    event.getJDA().getGuildById("514460308488716288").getController().createTextChannel(event.getAuthor().getId()).setTopic("5").complete();
-                }else {
-                    int VALUE = Integer.parseInt(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getTopic());
-                    VALUE++;
-                    event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getManager().setTopic(String.valueOf(VALUE)).queue();
-                }
-            }else {
-
-            }
-
-        });
 
     }
 }
