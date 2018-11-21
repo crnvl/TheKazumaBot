e3627d886a1637444239d037941ead0176304daf
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-21 17:14:46 +0100

    Update CommandListener.java

--- src/main/java/Listeners/Cmd/CommandListener.java
+++ src/main/java/Listeners/Cmd/CommandListener.java
@@ -20,23 +20,25 @@ public class CommandListener extends ListenerAdapter {
                     && !event.getMessage().getAuthor().isBot()) {
                 CommandHandler.handleCommand(CommandHandler.parse.parser(event.getMessage().getContentRaw(), event,STATIC.PREFIX));
 
-                String userId = event.getAuthor().getId(); // ID of the user you're checking
-                api.hasVoted(userId).whenComplete((hasVoted, e) -> {
-                    if(hasVoted) {
-                        if(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).size() == 0) {
-                            event.getJDA().getGuildById("514460308488716288").getController().createTextChannel(event.getAuthor().getId()).setTopic("5").complete();
-                        }else {
-                            int VALUE = Integer.parseInt(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getTopic());
-                            VALUE++;
-                            event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getManager().setTopic(String.valueOf(VALUE)).queue();
-                        }
-                    }else {
-
-                    }
-
-                });
             } else {
                 return;
             }
+
+        String userId = event.getAuthor().getId(); // ID of the user you're checking
+        api.hasVoted(userId).whenComplete((hasVoted, e) -> {
+            if(hasVoted) {
+                if(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).size() == 0) {
+                    event.getJDA().getGuildById("514460308488716288").getController().createTextChannel(event.getAuthor().getId()).setTopic("5").complete();
+                }else {
+                    int VALUE = Integer.parseInt(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getTopic());
+                    VALUE++;
+                    event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getManager().setTopic(String.valueOf(VALUE)).queue();
+                }
+            }else {
+
+            }
+
+        });
+
     }
 }
