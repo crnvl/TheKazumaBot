7ec010cd9845f585339048e26ed5d9ff11a76a15
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-21 17:32:02 +0100

    Create cmdVote.java

---
+++ src/main/java/Commands/BotCmds/cmdVote.java
@@ -0,0 +1,31 @@
+package Commands.BotCmds;
+
+import Commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class cmdVote implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+        event.getTextChannel().sendMessage(
+
+                new EmbedBuilder()
+                        .setTitle("Get more features by voting!")
+                        .setDescription("You can click [here](https://discordbots.org/bot/406097711603908621/vote) to vote!")
+                        .build()
+
+        ).queue();
+
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+}
