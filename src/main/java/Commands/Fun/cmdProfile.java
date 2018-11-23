6f8cb2f5551132e569a80dd89e9ebb35ac4e289c
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-23 18:10:50 +0100

    Update cmdProfile.java

--- src/main/java/Commands/Fun/cmdProfile.java
+++ src/main/java/Commands/Fun/cmdProfile.java
@@ -36,20 +36,38 @@ public class cmdProfile implements Command {
                 int VALUE = Integer.parseInt(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getTopic());
                 XP = String.valueOf(VALUE);
             }
-            event.getTextChannel().sendMessage(
+            String userId = event.getAuthor().getId(); // ID of the user you're checking
+            api.hasVoted(userId).whenComplete((hasVoted, e) -> {
+                if (hasVoted){
+                    event.getTextChannel().sendMessage(
 
-                    new EmbedBuilder()
-                            .setTitle(event.getMessage().getAuthor().getName() + "'s Profile Card")
-                            .addField(":star: Status", SET, true)
-                            .addField("Birthday", event.getAuthor().getCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
-                            .addField("Server Join", event.getMember().getJoinDate().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
-                            .addField("Level", XP + " XP", true)
-                            .addField("Vote", "[Click for extended features!](https://discordbots.org/bot/406097711603908621/vote)", true)
-                            .setThumbnail(event.getAuthor().getAvatarUrl())
-                            .setFooter("You need to vote to earn XP for 12h or to set your profile status!", null)
-                            .build()
+                            new EmbedBuilder()
+                                    .setTitle(event.getMessage().getAuthor().getName() + "'s Profile Card")
+                                    .addField(":star: Status", SET, true)
+                                    .addField("Birthday", event.getAuthor().getCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
+                                    .addField("Server Join", event.getMember().getJoinDate().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
+                                    .addField("Level", XP + " XP", true)
+                                    .setThumbnail(event.getAuthor().getAvatarUrl())
+                                    .build()
 
-            ).queue();
+                    ).queue();
+                }else {
+                    event.getTextChannel().sendMessage(
+
+                            new EmbedBuilder()
+                                    .setTitle(event.getMessage().getAuthor().getName() + "'s Profile Card")
+                                    .addField(":star: Status", SET, true)
+                                    .addField("Birthday", event.getAuthor().getCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
+                                    .addField("Server Join", event.getMember().getJoinDate().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
+                                    .addField("Level", XP + " XP", true)
+                                    .addField("Vote", "[Click for extended features!](https://discordbots.org/bot/406097711603908621/vote)", true)
+                                    .setThumbnail(event.getAuthor().getAvatarUrl())
+                                    .setFooter("You need to vote to earn XP for 12h or to set your profile status!", null)
+                                    .build()
+
+                    ).queue();
+                }});
+
 
         }else {
             switch (args[0]) {
@@ -111,7 +129,34 @@ public class cmdProfile implements Command {
                         ).queue();
                     }
                     break;
+
             }
+        }if(event.getMessage().getMentionedMembers().size() == 1) {
+            String SET, XP;
+            if (event.getJDA().getGuildById("514431614210670592").getTextChannelsByName(event.getMessage().getMentionedMembers().get(0).getUser().getId(), true).size() == 0){
+                SET = "None";
+            }else {
+                SET = event.getJDA().getGuildById("514431614210670592").getTextChannelsByName(event.getMessage().getMentionedMembers().get(0).getUser().getId(), true).get(0).getTopic();
+            }
+            if(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getMessage().getMentionedMembers().get(0).getUser().getId(), true).size() == 0) {
+                XP = "0";
+            }else {
+                int VALUE = Integer.parseInt(event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getMessage().getMentionedMembers().get(0).getUser().getId(), true).get(0).getTopic());
+                XP = String.valueOf(VALUE);
+            }
+            event.getTextChannel().sendMessage(
+
+                    new EmbedBuilder()
+                            .setTitle(event.getMessage().getMentionedMembers().get(0).getEffectiveName() + "'s Profile Card")
+                            .addField(":star: Status", SET, true)
+                            .addField("Birthday", event.getMessage().getMentionedMembers().get(0).getUser().getCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
+                            .addField("Server Join", event.getMessage().getMentionedMembers().get(0).getJoinDate().format(DateTimeFormatter.RFC_1123_DATE_TIME), true)
+                            .addField("Level", XP + " XP", true)
+                            .setThumbnail(event.getMessage().getMentionedMembers().get(0).getUser().getAvatarUrl())
+                            .build()
+
+            ).queue();
+
         }
 
     }
