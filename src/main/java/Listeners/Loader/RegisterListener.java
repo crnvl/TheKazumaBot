ddde0be625ac9b51b6a904ac2557daeff1030c7c
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-27 17:46:30 +0100

    Update RegisterListener.java

--- src/main/java/Listeners/Loader/RegisterListener.java
+++ src/main/java/Listeners/Loader/RegisterListener.java
@@ -14,11 +14,13 @@ public class RegisterListener extends ListenerAdapter {
     @Override
     public void onReady(ReadyEvent event) {
         event.getJDA().addEventListener(new CommandListener());
+        ///**
         event.getJDA().addEventListener(new AuditLogListener());
         event.getJDA().addEventListener(new MemberChangeListener());
         event.getJDA().addEventListener(new PrivateResponder());
         event.getJDA().addEventListener(new XPCreditsHandler());
        event.getJDA().addEventListener(new MentionResponder());
+       //**/
 
         event.getJDA().setAutoReconnect(true);
         System.out.println("Finished Loading!\n" +
