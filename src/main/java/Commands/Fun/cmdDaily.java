ca80278bebbb9468fe75052ef7197a0b1229d36e
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-27 17:41:51 +0100

    Create cmdDaily.java

---
+++ src/main/java/Commands/Fun/cmdDaily.java
@@ -0,0 +1,87 @@
+package Commands.Fun;
+
+import Commands.Command;
+import Core.Main;
+import Util.STATIC;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import org.discordbots.api.client.DiscordBotListAPI;
+
+import java.util.Random;
+
+public class cmdDaily implements Command {
+    DiscordBotListAPI api = new DiscordBotListAPI.Builder()
+            .token(System.getenv("DB_TOKEN"))
+            .botId("406097711603908621")
+            .build();
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+        //515083259957346304
+        //int countdownSeconds = 86400;
+        int countdownSeconds = 86400000;
+        long timestamp = System.currentTimeMillis();
+        long userId = event.getAuthor().getIdLong();
+        String userIdapi = event.getAuthor().getId(); // ID of the user you're checking
+        api.hasVoted(userIdapi).whenComplete((hasVoted, e) -> {
+            if (hasVoted) {
+                if (event.getJDA().getGuildById("514460308488716288").getTextChannelsByName(event.getAuthor().getId(), true).size() == 0) {
+                    if (Main.cooldowns.containsKey(userId) && timestamp - (Long) Main.cooldowns.get(userId) < countdownSeconds) {
+                        event.getTextChannel().sendMessage(
+
+                                new EmbedBuilder()
+                                        .setTitle("Action denied!")
+                                        .setDescription("You are just able to claim your daily every 24 hours!")
+                                        .build()
+
+                        ).queue();
+                    } else {
+
+                        Random rate = new Random();
+                        int r = rate.nextInt(30) + 120;
+                        if (event.getJDA().getGuildById("515083259957346304").getTextChannelsByName(event.getAuthor().getId(), true).size() == 0) {
+                            event.getJDA().getGuildById("515083259957346304").getController().createTextChannel(event.getAuthor().getId()).setTopic(String.valueOf(r)).complete();
+                        } else {
+                            event.getJDA().getGuildById("515083259957346304").getTextChannelsByName(event.getAuthor().getId(), true).get(0).getManager().setTopic(String.valueOf(r)).queue();
+                        }
+                        event.getTextChannel().sendMessage(
+
+                                new EmbedBuilder()
+                                        .setTitle("Daily claimed!")
+                                        .setDescription("You recieved " + r + " credits")
+                                        .build()
+
+                        ).queue();
+                        Main.cooldowns.put(userId, timestamp);
+                    }
+
+
+                } else {
+
+                }
+
+            } else {
+                event.getTextChannel().sendMessage(
+
+                        new EmbedBuilder()
+                                .setTitle("Access denied!")
+                                .setDescription("You need to vote for this bot to use this Command!\n" +
+                                        "You can vote [here](https://discordbots.org/bot/406097711603908621/vote)")
+                                .build()
+
+                ).queue();
+            }
+        });
+    }
+
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+}
