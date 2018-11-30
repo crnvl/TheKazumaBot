120dd4cded698f038716e4004e53baf2e03e074b
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-11-30 18:02:57 +0100

    Update requestGetUser.java

--- src/main/java/Commands/Games/OsuRequest/requestGetUser.java
+++ src/main/java/Commands/Games/OsuRequest/requestGetUser.java
@@ -28,7 +28,7 @@ public class requestGetUser implements Command {
     public void action(String[] args, MessageReceivedEvent event) {
 
         // Create a new Osu object with an API key
-        if(args.length < 2) {
+        if(args.length == 0) {
             event.getTextChannel().sendMessage(
 
                     new EmbedBuilder()
@@ -41,63 +41,146 @@ public class requestGetUser implements Command {
             String USER = args[0];
             if (args[1].contains("mania")) {
                 GameMode MODE = GameMode.MANIA;
+                String key = System.getenv("OSU_KEY");
+                Osu osu = Osu.getAPI(key);
+                try {
+                    OsuUser user = osu.users.query(new EndpointUsers.ArgumentsBuilder(USER).setMode(MODE).build());
+                    event.getTextChannel().sendMessage(
+
+                            new EmbedBuilder()
+                                    .setColor(Color.PINK)
+                                    .setTitle("Osu profile of " + user.getUsername())
+                                    .addField("ID", "``" + user.getID() + "``", false)
+                                    .addField("Country", "``" + user.getCountry() + "``", true)
+                                    .addField("Accuracy", "``" + user.getAccuracy() + "%``", true)
+                                    .addField("Played Maps", "``" + user.getPlayCount() + "``", true)
+                                    .addField("Total Score", "``" + user.getTotalScore() + "``", false)
+                                    .addField("User URL", "``" + user.getURL() + "``", false)
+                                    .addField("Ranking", "``#" + user.getRank() + "(" + user.getPP() + "pp) Country Ranking: #" + user.getCountryRank() + "``", false)
+                                    .addField("Ranks", "``SS+: " + user.getCountRankSSH() + ", SS: " + user.getCountRankSS() + ", S+: " + user.getCountRankSH() + ", S: " + user.getCountRankS() + ", A: " + user.getCountRankA() + "``", false)
+                                    .build()
+
+                    ).queue();
+
+                } catch (OsuAPIException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             } else if (args[1].contains("taiko")) {
                 GameMode MODE = GameMode.TAIKO;
+                String key = System.getenv("OSU_KEY");
+                Osu osu = Osu.getAPI(key);
+                try {
+                    OsuUser user = osu.users.query(new EndpointUsers.ArgumentsBuilder(USER).setMode(MODE).build());
+                    event.getTextChannel().sendMessage(
+
+                            new EmbedBuilder()
+                                    .setColor(Color.PINK)
+                                    .setTitle("Osu profile of " + user.getUsername())
+                                    .addField("ID", "``" + user.getID() + "``", false)
+                                    .addField("Country", "``" + user.getCountry() + "``", true)
+                                    .addField("Accuracy", "``" + user.getAccuracy() + "%``", true)
+                                    .addField("Played Maps", "``" + user.getPlayCount() + "``", true)
+                                    .addField("Total Score", "``" + user.getTotalScore() + "``", false)
+                                    .addField("User URL", "``" + user.getURL() + "``", false)
+                                    .addField("Ranking", "``#" + user.getRank() + "(" + user.getPP() + "pp) Country Ranking: #" + user.getCountryRank() + "``", false)
+                                    .addField("Ranks", "``SS+: " + user.getCountRankSSH() + ", SS: " + user.getCountRankSS() + ", S+: " + user.getCountRankSH() + ", S: " + user.getCountRankS() + ", A: " + user.getCountRankA() + "``", false)
+                                    .build()
+
+                    ).queue();
+
+                } catch (OsuAPIException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             } else if (args[1].contains("ctb")) {
                 GameMode MODE = GameMode.CATCH_THE_BEAT;
+                String key = System.getenv("OSU_KEY");
+                Osu osu = Osu.getAPI(key);
+                try {
+                    OsuUser user = osu.users.query(new EndpointUsers.ArgumentsBuilder(USER).setMode(MODE).build());
+                    event.getTextChannel().sendMessage(
+
+                            new EmbedBuilder()
+                                    .setColor(Color.PINK)
+                                    .setTitle("Osu profile of " + user.getUsername())
+                                    .addField("ID", "``" + user.getID() + "``", false)
+                                    .addField("Country", "``" + user.getCountry() + "``", true)
+                                    .addField("Accuracy", "``" + user.getAccuracy() + "%``", true)
+                                    .addField("Played Maps", "``" + user.getPlayCount() + "``", true)
+                                    .addField("Total Score", "``" + user.getTotalScore() + "``", false)
+                                    .addField("User URL", "``" + user.getURL() + "``", false)
+                                    .addField("Ranking", "``#" + user.getRank() + "(" + user.getPP() + "pp) Country Ranking: #" + user.getCountryRank() + "``", false)
+                                    .addField("Ranks", "``SS+: " + user.getCountRankSSH() + ", SS: " + user.getCountRankSS() + ", S+: " + user.getCountRankSH() + ", S: " + user.getCountRankS() + ", A: " + user.getCountRankA() + "``", false)
+                                    .build()
+
+                    ).queue();
+
+                } catch (OsuAPIException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             } else if (args[1].contains("standard")) {
                 GameMode MODE = GameMode.STANDARD;
+                String key = System.getenv("OSU_KEY");
+                Osu osu = Osu.getAPI(key);
+                try {
+                    OsuUser user = osu.users.query(new EndpointUsers.ArgumentsBuilder(USER).setMode(MODE).build());
+                    event.getTextChannel().sendMessage(
+
+                            new EmbedBuilder()
+                                    .setColor(Color.PINK)
+                                    .setTitle("Osu profile of " + user.getUsername())
+                                    .addField("ID", "``" + user.getID() + "``", false)
+                                    .addField("Country", "``" + user.getCountry() + "``", true)
+                                    .addField("Accuracy", "``" + user.getAccuracy() + "%``", true)
+                                    .addField("Played Maps", "``" + user.getPlayCount() + "``", true)
+                                    .addField("Total Score", "``" + user.getTotalScore() + "``", false)
+                                    .addField("User URL", "``" + user.getURL() + "``", false)
+                                    .addField("Ranking", "``#" + user.getRank() + "(" + user.getPP() + "pp) Country Ranking: #" + user.getCountryRank() + "``", false)
+                                    .addField("Ranks", "``SS+: " + user.getCountRankSSH() + ", SS: " + user.getCountRankSS() + ", S+: " + user.getCountRankSH() + ", S: " + user.getCountRankS() + ", A: " + user.getCountRankA() + "``", false)
+                                    .build()
+
+                    ).queue();
+
+                } catch (OsuAPIException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             } else {
                 GameMode MODE = GameMode.STANDARD;
+                String key = System.getenv("OSU_KEY");
+                Osu osu = Osu.getAPI(key);
+                try {
+                    OsuUser user = osu.users.query(new EndpointUsers.ArgumentsBuilder(USER).setMode(MODE).build());
+                    event.getTextChannel().sendMessage(
+
+                            new EmbedBuilder()
+                                    .setColor(Color.PINK)
+                                    .setTitle("Osu profile of " + user.getUsername())
+                                    .addField("ID", "``" + user.getID() + "``", false)
+                                    .addField("Country", "``" + user.getCountry() + "``", true)
+                                    .addField("Accuracy", "``" + user.getAccuracy() + "%``", true)
+                                    .addField("Played Maps", "``" + user.getPlayCount() + "``", true)
+                                    .addField("Total Score", "``" + user.getTotalScore() + "``", false)
+                                    .addField("User URL", "``" + user.getURL() + "``", false)
+                                    .addField("Ranking", "``#" + user.getRank() + "(" + user.getPP() + "pp) Country Ranking: #" + user.getCountryRank() + "``", false)
+                                    .addField("Ranks", "``SS+: " + user.getCountRankSSH() + ", SS: " + user.getCountRankSS() + ", S+: " + user.getCountRankSH() + ", S: " + user.getCountRankS() + ", A: " + user.getCountRankA() + "``", false)
+                                    .build()
+
+                    ).queue();
+
+                } catch (OsuAPIException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
-            GameMode MODE = GameMode.STANDARD;
-            String key = System.getenv("OSU_KEY");
-            Osu osu = Osu.getAPI(key);
-            try {
-                OsuUser user = osu.users.query(new EndpointUsers.ArgumentsBuilder(USER).setMode(MODE).build());
 
-
-                event.getTextChannel().sendMessage(
-
-                        new EmbedBuilder()
-                                .setColor(Color.PINK)
-                                .setTitle("Osu profile of " + user.getUsername())
-                                .addField("ID", "``" + user.getID() + "``", false)
-                                .addField("Country", "``" + user.getCountry() + "``", true)
-                                .addField("Accuracy", "``" + user.getAccuracy() + "%``", true)
-                                .addField("Played Maps", "``" + user.getPlayCount() + "``", true)
-                                .addField("Total Score", "``" + user.getTotalScore() + "``", false)
-                                .addField("User URL", "``" + user.getURL() + "``", false)
-                                .addField("Ranking", "``#" + user.getRank() + "(" + user.getPP() + "pp) Country Ranking: #" + user.getCountryRank() + "``", false)
-                                .addField("Total Score", "``" + user.getTotalScore() + "``", false)
-                                .addField("Ranks", "``SS+: " + user.getCountRankSSH() + ", SS: " + user.getCountRankSS() + ", S+: " + user.getCountRankSH() + ", S: " + user.getCountRankS() + ", A: " + user.getCountRankA() + "``", false)
-                                .build()
-
-                ).queue();
-
-            } catch (OsuAPIException e) {
-                event.getTextChannel().sendMessage(
-
-                        new EmbedBuilder()
-                                .setTitle("Osu API Exception")
-                                .setDescription("It seems like there was an exception while executing this command! Did you type everything correctly?\n" +
-                                        "Please try again or contact our staff team with ``" + STATIC.PREFIX + "report <your report description>``")
-                                .addField("Error Code", "GenO1", true)
-                                .build()
-
-                ).queue();
-            } catch (MalformedURLException e) {
-                event.getTextChannel().sendMessage(
-
-                        new EmbedBuilder()
-                                .setTitle("Osu API Exception")
-                                .setDescription("It seems like there was an exception while executing this command!\n" +
-                                        "Please try again or contact our staff team with ``" + STATIC.PREFIX + "report <your report description>``")
-                                .addField("Error Code", "GenO2", true)
-                                .build()
-
-                ).queue();
-            }
         }
     }
 
