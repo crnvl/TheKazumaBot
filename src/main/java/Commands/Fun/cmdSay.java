43b73d4748f8a8a2a0b570de0545b46545cc681d
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-12-03 14:28:37 +0100

    Create cmdSay.java

---
+++ src/main/java/Commands/Fun/cmdSay.java
@@ -0,0 +1,33 @@
+package Commands.Fun;
+
+import Commands.Command;
+import Util.STATIC;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class cmdSay implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+        String MESSAGE = event.getMessage().getContentRaw().replace(STATIC.PREFIX + "say ", "");
+
+        event.getTextChannel().sendMessage(
+
+                new EmbedBuilder()
+                        .setDescription(MESSAGE)
+                .build()
+
+        ).queue();
+
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+}
