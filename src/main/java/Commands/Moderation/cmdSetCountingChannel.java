9ba01ae4c36dd4eeeebc8de9012767ac85fa82ee
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-12-09 12:43:12 +0100

    Update cmdSetCountingChannel.java

--- src/main/java/Commands/Moderation/cmdSetCountingChannel.java
+++ src/main/java/Commands/Moderation/cmdSetCountingChannel.java
@@ -76,7 +76,11 @@ public class cmdSetCountingChannel implements Command {
                 }else {
 
                     event.getJDA().getGuildById("519454815806554112").getTextChannelsByName(event.getGuild().getId(), true).get(0).getManager().setTopic(event.getMessage().getMentionedChannels().get(0).getAsMention()).queue();
-                    event.getJDA().getGuildById("521278542063992832").getTextChannelsByName(event.getGuild().getId(), true).get(0).getManager().setTopic("1").queue();
+                    if(event.getJDA().getGuildById("521278542063992832").getTextChannelsByName(event.getGuild().getId(), true).size() == 0) {
+                        event.getJDA().getGuildById("521278542063992832").getController().createTextChannel(event.getGuild().getId()).setTopic("1").queue();
+                    }else {
+                        event.getJDA().getGuildById("521278542063992832").getTextChannelsByName(event.getGuild().getId(), true).get(0).getManager().setTopic("1").queue();
+                    }
                     event.getTextChannel().sendMessage(
 
                             new EmbedBuilder()
