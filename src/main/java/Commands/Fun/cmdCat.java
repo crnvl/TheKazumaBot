d9e7fc906ef4f2b52e18a40079c22bd1b257b59a
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-12-12 17:23:32 +0100

    Create cmdCat.java

---
+++ src/main/java/Commands/Fun/cmdCat.java
@@ -0,0 +1,40 @@
+package Commands.Fun;
+
+import Commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.awt.*;
+import java.io.IOException;
+
+import static Util.Tools.readJsonArrayFromUrl;
+
+public class cmdCat implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+        try {
+            String url = "";
+            JSONArray jsonArray = readJsonArrayFromUrl("https://api.thecatapi.com/v1/images/search");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonobject = jsonArray.getJSONObject(i);
+                url = jsonobject.getString("url");
+            }
+            EmbedBuilder builder = new EmbedBuilder().setTitle("a cat for " + event.getMessage().getAuthor().getName()).setImage(url).setColor(Color.YELLOW);
+            event.getTextChannel().sendMessage(builder.build()).queue();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+}
