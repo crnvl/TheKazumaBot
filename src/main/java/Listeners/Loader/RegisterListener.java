1eedcdc74299a61fbc505d5bbfe2fe8c66a792eb
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-12-15 18:18:01 +0100

    Update RegisterListener.java

--- src/main/java/Listeners/Loader/RegisterListener.java
+++ src/main/java/Listeners/Loader/RegisterListener.java
@@ -3,6 +3,7 @@ package Listeners.Loader;
 import Listeners.Announcements.CountingChannelListener;
 import Listeners.Announcements.MemberChangeListener;
 import Listeners.Cmd.CommandListener;
+import Listeners.GlobalChat.GCListener;
 import Listeners.Logs.AuditLogListener;
 import Listeners.MentionResponder;
 import Listeners.Private.PrivateResponder;
@@ -15,13 +16,13 @@ public class RegisterListener extends ListenerAdapter {
     @Override
     public void onReady(ReadyEvent event) {
         event.getJDA().addEventListener(new CommandListener());
-
         event.getJDA().addEventListener(new AuditLogListener());
         event.getJDA().addEventListener(new MemberChangeListener());
         event.getJDA().addEventListener(new PrivateResponder());
         event.getJDA().addEventListener(new XPCreditsHandler());
        event.getJDA().addEventListener(new MentionResponder());
         event.getJDA().addEventListener(new CountingChannelListener());
+        event.getJDA().addEventListener(new GCListener());
 
 
         event.getJDA().setAutoReconnect(true);
