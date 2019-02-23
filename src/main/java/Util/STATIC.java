e9337019c34ecfeb430ef7b71fab6dc914f386ed
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-23 01:40:15 +0100

    Update STATIC.java

--- src/main/java/Util/STATIC.java
+++ src/main/java/Util/STATIC.java
@@ -2,7 +2,7 @@ package Util;
 
 public class STATIC {
 
-    public static final String VERSION = "8.2.0";
+    public static final String VERSION = "8.2.3";
 
     public static final String PREFIX = "/k ";
 }
