b1f6931a32c21330a603a9b32e543dd96162874d
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-26 18:30:05 +0100

    Update cmdBalance.java

--- src/main/java/Commands/Social/cmdBalance.java
+++ src/main/java/Commands/Social/cmdBalance.java
@@ -21,7 +21,7 @@ public class cmdBalance implements Command {
             }
             event.getTextChannel().sendMessage(event.getMessage().getAuthor().getAsMention() + ", **you currently have a balance of " + CREDITS + " Credits!** :yen:").queue();
         }else {
-            if(args.length == 2 && args[1].contains(event.getMessage().getMentionedMembers().get(0).getAsMention())) {
+            if(args.length == 2 && args[0].contains(event.getMessage().getMentionedMembers().get(0).getAsMention())) {
                 int cGive, cHave, cNew, cRest, cGivenNew;
                 String cMember, cId;
                 cId = event.getMessage().getMentionedMembers().get(0).getUser().getId();
