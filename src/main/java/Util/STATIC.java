ae12052bb0731b65f66966894b10b199e275015f
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-26 18:19:31 +0100

    Update STATIC.java

--- src/main/java/Util/STATIC.java
+++ src/main/java/Util/STATIC.java
@@ -1,8 +1,12 @@
 package Util;
 
+import java.awt.*;
+
 public class STATIC {
 
-    public static final String VERSION = "8.2.3";
+    public static final String VERSION = "8.2.4";
 
     public static final String PREFIX = "/k ";
+
+    public static final Color MAIN = new Color(88, 176, 125);
 }
