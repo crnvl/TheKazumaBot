2c3c7ebe6e1aa3496be7a2f9863389f89722fdfc
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-28 16:56:03 +0100

    Update cmdOwofy.java

--- src/main/java/Commands/Fun/cmdOwofy.java
+++ src/main/java/Commands/Fun/cmdOwofy.java
@@ -2,9 +2,12 @@ package Commands.Fun;
 
 import Commands.Command;
 import Util.STATIC;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import pw.aru.api.nekos4j.Nekos4J;
 import pw.aru.api.nekos4j.text.TextProvider;
+
+import java.awt.*;
 
 public class cmdOwofy implements Command {
 
@@ -16,12 +19,21 @@ public class cmdOwofy implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
         Nekos4J api = new Nekos4J.Builder().build();
-
-        TextProvider textProvider = api.getTextProvider();
-        String message = event.getMessage().getContentRaw().replace(STATIC.PREFIX + " owofy", "");
-        textProvider.owoifyText(message).async(text->{
-            event.getTextChannel().sendMessage("**" + text + "**").queue();
-        });
+        if(args.length != 0) {
+            TextProvider textProvider = api.getTextProvider();
+            String message = event.getMessage().getContentRaw().replace(STATIC.PREFIX + " owofy", "");
+            textProvider.owoifyText(message).async(text -> {
+                event.getTextChannel().sendMessage("**" + message + "**").queue();
+            });
+        }else {
+            event.getTextChannel().sendMessage(
+                    new EmbedBuilder()
+                            .setTitle("Please specify your request!")
+                            .setColor(Color.RED)
+                            .setDescription("Please use this command like this: ``/k owofy <text>``")
+                            .build()
+            ).queue();
+        }
 
     }
 
