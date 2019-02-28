09655b7880de4a2b7939702e5c6fec37d2d75ca9
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-28 17:08:07 +0100

    Update cmdOwofy.java

--- src/main/java/Commands/Fun/cmdOwofy.java
+++ src/main/java/Commands/Fun/cmdOwofy.java
@@ -22,7 +22,6 @@ public class cmdOwofy implements Command {
         if(args.length != 0) {
             TextProvider textProvider = api.getTextProvider();
             String messageGet = event.getMessage().getContentRaw().replace("/k owofy", "");
-            System.out.println(messageGet);
             textProvider.owoifyText(messageGet).async(text -> {
                 event.getTextChannel().sendMessage( event.getAuthor().getAsMention() + ", your Text is: **" + text + "**").queue();
             });
