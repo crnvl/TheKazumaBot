14479d2c0a7067a4128e818a9d17974e5d07ff92
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-03-05 16:13:48 +0100

    Update cmdStatistics.java

--- src/main/java/Commands/DiscordBotsOrg/cmdStatistics.java
+++ src/main/java/Commands/DiscordBotsOrg/cmdStatistics.java
@@ -1,6 +1,7 @@
 package Commands.DiscordBotsOrg;
 
 import Commands.Command;
+import Util.STATIC;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.discordbots.api.client.DiscordBotListAPI;
@@ -22,6 +23,7 @@ public class cmdStatistics implements Command {
 
         event.getTextChannel().sendMessage(
                 new EmbedBuilder()
+                        .setColor(STATIC.MAIN)
                         .setTitle("My Statistics on discordbots.org", "https://discordbots.org")
                         .addField("Vote for me!", "[Vote on discordbots.org](https://discordbots.org/bot/406097711603908621)", false)
                         .setImage("https://discordbots.org/api/widget/406097711603908621.png?usernamecolor=FFFFFF&topcolor=000000&ran=" + y).build()).queue();
