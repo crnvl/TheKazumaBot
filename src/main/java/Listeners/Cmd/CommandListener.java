7091f27cd53cdc69eb99df746de5a4390815a12b
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-03-25 18:25:21 +0100

    Update CommandListener.java

--- src/main/java/Listeners/Cmd/CommandListener.java
+++ src/main/java/Listeners/Cmd/CommandListener.java
@@ -1,5 +1,6 @@
 package Listeners.Cmd;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import Core.Execute.CommandHandler;
@@ -15,7 +16,24 @@ public class CommandListener extends ListenerAdapter {
             if (event.getMessage().getContentRaw().startsWith(STATIC.PREFIX)
                     && !Objects.equals(event.getMessage().getAuthor().getId(), event.getJDA().getSelfUser().getId())
                     && !event.getMessage().getAuthor().isBot()) {
-                CommandHandler.handleCommand(CommandHandler.parse.parser(event.getMessage().getContentRaw(), event,STATIC.PREFIX));
+
+
+                if (event.getJDA().getGuildById("559488253376462878").getTextChannelsByName(event.getAuthor().getId(), true).size() == 0) {
+
+
+                    CommandHandler.handleCommand(CommandHandler.parse.parser(event.getMessage().getContentRaw(), event, STATIC.PREFIX));
+
+                } else {
+                    event.getTextChannel().sendMessage(
+                            new EmbedBuilder()
+                                    .setTitle(event.getAuthor().getName() + " you are banned from Kazuma")
+                                    .setDescription("Due to inappropriate Actions in our Globalchat, you're temporarily banned from acting as a participant. If you want to get your access back, apply on the official discord server")
+                                    .addField("Support", "If this is an error please contact us. [join our sopport server](https://discord.gg/fyJ6PFj)", false).build()
+
+
+
+                    ).queue();
+                }
 
             } else {
                 return;
